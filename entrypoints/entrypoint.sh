fcd3e769bb765b66008e7aeb429dd4ff75c911b4
ashraya.tuladhar <ashraya.tuladhar@tekvortex.com>
2024-08-13 14:47:00 +0545

    updated envs

--- entrypoints/entrypoint.sh
+++ entrypoints/entrypoint.sh
@@ -6,20 +6,6 @@ chmod +x /app/entrypoints/start.sh
 chmod +x /app/entrypoints/end.sh
 chmod +x /app/build.sh
 
-TECHNOLOGY=javascript
-NODE_VERSION=18.20.4
-HOSTNAME=3d45ef4f2d9f
-YARN_VERSION=1.22.19
-SHLVL=1
-HOME=/root
-VERSION=18
-TERM=xterm
-SERVICE_NAME=build-service-1
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-BUILD_ID=bd1ac2d5-c4c4-40e8-8e0f-0add5cc2630d
-MANIFEST_FILE=package.json
-PWD=/app
-
 sh -c /app/entrypoints/setup.sh
 sh -c /app/entrypoints/certificate.sh
 sh -c /app/entrypoints/start.sh
